42318c9c7c80cc3ea2c97a9f350e13f57aae05f7
James Cameron <james.cameron@coxautoinc.com>
2022-02-03 07:35:07 -0500

    fixed some global css that disabled scrolling

--- theme/global.ts
+++ theme/global.ts
@@ -1,28 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    *, *:before, *:after {
+    * {
         box-sizing: border-box;
-    }
-    html {
-        box-sizing: border-box;
-        scroll-behavior: smooth;
-        font-size: 100%;
-        
+        padding: 0;
+        margin: 0;
     }
     body {
-         font-family: 'Lato', sans-serif;
-         font-size: 16px;
-         background: white;
-         color: ${(props) => props.theme.colors.tertiary};
+        background: ${({ theme }) => theme.body};
+        color: ${({ theme }) => theme.text};
+        font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
+        transition: all 0.50s linear;
+        background: #fff;
     }
-    a {
-        text-decoration: none; 
-    }
-    @media (min-width: 860px) {
-    font-size: 18px;
-  }
-`
-
+`;
 
 export default GlobalStyle;
