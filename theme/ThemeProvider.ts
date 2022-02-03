668518c37b15714d0f5e36c44d291e1398707e8d
James Cameron <james.cameron@coxautoinc.com>
2022-02-03 07:25:31 -0500

    was importing native version of styled components.  caused a break in the build. fixed

--- theme/ThemeProvider.ts
+++ theme/ThemeProvider.ts
@@ -1,5 +1,5 @@
 import styled, { createGlobalStyle } from "styled-components"
-import { DefaultTheme } from "styled-components/native";
+import { DefaultTheme } from "styled-components";
 
 declare module 'styled-components' {
   export interface DefaultTheme {
